d9697097add2aa1748f1b1a8f29cd2a931095af1
Mesut Ozil <matcortnermat@gmail.com>
2022-04-09 11:58:32 +0530

    Added credentials and handled bootstrap

---
+++ app/models/stock.rb
@@ -0,0 +1,10 @@
+class Stock < ApplicationRecord
+    def self.new_lookup(ticker_name)
+        client = IEX::Api::Client.new(
+            publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+            secret_token: Rails.application.credentials.iex_client[:secret_token],
+            endpoint: 'https://cloud.iexapis.com/v1'
+        )
+        client.price(ticker_name)
+    end
+end
