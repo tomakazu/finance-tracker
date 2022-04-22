846211133bdc23dfc9c6bbb4404d8196237df4df
Mesut Ozil <matcortnermat@gmail.com>
2022-04-23 02:26:28 +0530

    Cleaned up the code

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,10 @@ class Stock < ApplicationRecord
             secret_token: Rails.application.credentials.iex_client[:secret_token],
             endpoint: 'https://cloud.iexapis.com/v1'
         )
+        has_many :user_stocks
+        has_many :users, through: :user_stocks
+        
+        validates :name, :ticker_symbol, presence: true
         begin
             new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol)) 
         rescue => exception
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
   root 'welcome#index'
