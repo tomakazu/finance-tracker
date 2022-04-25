1d9aeca86474dcf0cde435d9772fa17373cfc9c7
Mesut Ozil <matcortnermat@gmail.com>
2022-04-25 23:57:47 +0530

    first name and last name parameters are added to devise user

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,12 @@ class UserStocksController < ApplicationController
             redirect_to my_portfolio_path
         end
     end
+
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user, stock_id: stock.id)
+        user_stock.destroy
+        flash[:notice] = "#{stock.name} was successfully removed from portfolio"
+        redirect_to my_portfolio_path
+    end
 end
