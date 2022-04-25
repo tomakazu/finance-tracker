ba89053f1eee0d5b7159cd75b87c311876863e8f
Mesut Ozil <matcortnermat@gmail.com>
2022-04-25 14:39:02 +0530

    Added restriction on adding stocks

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+  def can_track_stock?(ticker_symbol)
+    return under_stock_limit? && !stock_already_tracked(ticker_symbol)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_stocks
+  resources :user_stocks, only: [:create]
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
   root 'welcome#index'
